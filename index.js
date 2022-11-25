53af42601782eb4ec5c8f695b826effecb73fecc
philip <philmbugua1@gmail.com>
2022-11-25 15:56:49 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(theDistance){
+  if (theDistance <=400){
+    return "This one is on me!";
+  } 
+  else if (theDistance >2000&& theDistance <2500){
+    return "I will gladly take your thirty bucks.";
+  }
+  else if (theDistance >2500){
+    return "No can do.";
+  }
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === "NYC"){
+    return "Ok, sounds good.";
+  }
+  else if (city !== "NYC"){
+    return "No go.";
+  }
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if ( tip === "generous") {
+    return "Thank you so much.";
+  }
+  else if ( tip === "not as generous") {
+    return "Thank you.";
+  }
+  else {
+    return "Bye.";
+  }
 }
